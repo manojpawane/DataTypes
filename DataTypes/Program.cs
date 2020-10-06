5acf09cd4722df16f272daee9d9d6f74fe2b8ef1
manojpawane <manojpawane@gmail.com>
2020-10-06 18:41:58 +0530

    [Manoj] Add code

---
+++ DataTypes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+
+            int ch = 0;
+            do
+            {
+                Console.WriteLine("1. Addition");
+                Console.WriteLine("2. Substraction");
+                Console.WriteLine("3. Division");
+                Console.WriteLine("4. Multiplication");
+                Console.WriteLine("5. Exit..");
+                ch = Convert.ToInt32(Console.ReadLine());
+                switch(ch)
+                {
+                    case 1: Console.WriteLine("Add");
+                        break;
+                    case 2:
+                        Console.WriteLine("Substraction");
+                        break;
+                    case 3:
+                        Console.WriteLine("Divsion");
+                        break;
+                    case 4:
+                        Console.WriteLine("Multiplication");
+                        break;
+                }
+            }
+            while (ch != 5);
+            
+        }
+    }
+}
